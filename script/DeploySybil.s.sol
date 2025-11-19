e8426470afe9ab34a9a915c1d296cab49579343c
Aryan Soni <aryan@tokamak.network>
2025-11-19 17:38:08 +0530

    feat: setup the contract folder

---
+++ script/DeploySybil.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "forge-std/Script.sol";
+import {Sybil} from "../src/Sybil.sol";
+import {Verifier} from "../src/Verifier.sol";
+
+contract FunctionScript is Script {
+
+    function run() external {
+        address verifier = vm.envAddress("VERIFIER");
+
+        // Specify Poseidon contract addresses
+        address poseidon1Elements = vm.envAddress("POSEIDON1ELEMENTS");
+        address poseidon2Elements = vm.envAddress("POSEIDON2ELEMENTS");
+        address poseidon3Elements = vm.envAddress("POSEIDON3ELEMENTS");
+        address adminRole = msg.sender;
+
+        vm.startBroadcast();
+        // Deploy the Sybil contract
+        Sybil sybilContract = new Sybil();
+
+        // Calling initialize at the time of deployment
+        sybilContract.initialize(
+            verifier,
+            poseidon1Elements,
+            poseidon2Elements,
+            poseidon3Elements,
+            adminRole
+        );
+
+        vm.stopBroadcast();
+
+        console2.log("Sybil contract is deployed at:", address(sybilContract));
+    }
+}
